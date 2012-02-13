ff886517a70e01ffa54adc3dfd726a9e54647be1
sauimone <samu.uimonen@digia.com>
2012-02-13 17:09:19 +0200

    updated barchart examples

--- example/stackedbarchart/main.cpp
+++ example/stackedbarchart/main.cpp
@@ -3,6 +3,8 @@
 #include <QStandardItemModel>
 #include <stackedbarchartseries.h>
 #include "chartwidget.h"
+#include <qbarset.h>
+#include <qbarcategory.h>
 
 QTCOMMERCIALCHART_USE_NAMESPACE
 
@@ -11,29 +13,29 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QMainWindow window;
 
-    StackedBarChartSeries* series0 = new StackedBarChartSeries();
+    QBarCategory category;
+    category << "Jan" << "Feb" << "Mar" << "Apr" << "May" << "June" << "Jul" << "Aug" << "Sep" << "Nov" << "Dec";
+
+    StackedBarChartSeries* series0 = new StackedBarChartSeries(category);
+
+    QBarSet barSet0;
+    QBarSet barSet1;
+    QBarSet barSet2;
+    QBarSet barSet3;
+    QBarSet barSet4;
 
     // Create some test data to chart
-    QList<qreal> data0;
-    data0 << 1 << 2 << 3 << 4 << 5 << 6 << 7 << 8 << 9 << 10;
-    QList<qreal> data1;
-    data1 << 5 << 0 << 0 << 4 << 0 << 7 << 8 << 9 << 9 << 0;
-    QList<qreal> data2;
-    data2 << 3 << 5 << 8 << 13 << 8 << 5 << 3 << 2 << 1 << 1;
-    QList<qreal> data3;
-    data3 << 5 << 6 << 7 << 3 << 4 << 5 << 8 << 9 << 10 << 5;
-    QList<qreal> data4;
-    data4 << 9 << 7 << 5 << 3 << 1 << 2 << 4 << 6 << 8 << 10;
+    barSet0 << 1 << 2 << 3 << 4 << 5 << 6 << 7 << 8 << 9 << 10;
+    barSet1 << 5 << 0 << 0 << 4 << 0 << 7 << 8 << 9 << 9 << 0;
+    barSet2 << 3 << 5 << 8 << 13 << 8 << 5 << 3 << 2 << 1 << 1;
+    barSet3 << 5 << 6 << 7 << 3 << 4 << 5 << 8 << 9 << 10 << 5;
+    barSet4 << 9 << 7 << 5 << 3 << 1 << 2 << 4 << 6 << 8 << 10;
 
-    series0->addData(data0);
-    series0->addData(data1);
-    series0->addData(data2);
-    series0->addData(data3);
-    series0->addData(data4);
-
-    QList<QString> labels;
-    labels << "Jan" << "Feb" << "Mar" << "Apr" << "May" << "June" << "Jul" << "Aug" << "Sep" << "Nov" << "Dec";
-    series0->setLabels(labels);
+    series0->addBarSet(barSet0);
+    series0->addBarSet(barSet1);
+    series0->addBarSet(barSet2);
+    series0->addBarSet(barSet3);
+    series0->addBarSet(barSet4);
 
     ChartWidget* chartWidget =  new ChartWidget(&window);
     chartWidget->addSeries(series0);
