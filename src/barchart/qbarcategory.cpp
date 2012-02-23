8c95548bc7163befdb171cf7c180d65ae0f2283a
sauimone <samu.uimonen@digia.com>
2012-02-23 17:01:54 +0200

    some qdoc pollution inserted to qbarcategory

--- src/barchart/qbarcategory.cpp
+++ src/barchart/qbarcategory.cpp
@@ -2,21 +2,43 @@
 
 QTCOMMERCIALCHART_BEGIN_NAMESPACE
 
+/*!
+    \class QBarCategory
+    \brief QtCommercial chart API.
+
+    QBarCategory is a container for labels of various bar charts. Before the
+    bar chart can be constructed, the categories must be defined. This is done by
+    creating a QBarCategory class and appending the labels of categories to it.
+    The QBarCategory is then given to QBarSeries class.
+*/
+
+/*!
+    Constructs the category container
+*/
 QBarCategory::QBarCategory()
 {
 }
 
+/*!
+    Appends the \a label in the category.
+*/
 QBarCategory& QBarCategory::operator << (const QString &label)
 {
     mList.append(label);
     return *this;
 }
 
+/*!
+    Retrurns number of labels in category
+*/
 int QBarCategory::count()
 {
     return mList.count();
 }
 
+/*!
+    Retruns the label of category defined by index \a category
+*/
 QString QBarCategory::label(int category)
 {
     return mList.at(category);
