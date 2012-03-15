9ab5701700a122d5566c296bb529526687ecbb65
Tero Ahola <tero.ahola@digia.com>
2012-03-15 13:46:04 +0200

    Colors of the darker theme

--- src/themes/chartthemebluecerulean_p.h
+++ src/themes/chartthemebluecerulean_p.h
@@ -8,27 +8,29 @@ public:
     ChartThemeBlueCerulean() : ChartTheme(QChart::ChartThemeBlueCerulean)
     {
         // Series colors
-        m_seriesColors << QRgb(0xc7e85b);
-        m_seriesColors << QRgb(0x5dbe9b);
-        m_seriesColors << QRgb(0x4fbef3);
+        m_seriesColors << QRgb(0x38ad6b);
+        m_seriesColors << QRgb(0x3c84a7);
+        m_seriesColors << QRgb(0xeb8817);
+        m_seriesColors << QRgb(0x7b7f8c);
+        m_seriesColors << QRgb(0xbf593e);
         generateSeriesGradients();
 
         // Background
         QLinearGradient backgroundGradient;
-        backgroundGradient.setColorAt(0.0, QRgb(0x056188));
-        backgroundGradient.setColorAt(1.0, QRgb(0x101a33));
+        backgroundGradient.setColorAt(0.0, QRgb(0xd6d6d6));
+        backgroundGradient.setColorAt(1.0, QRgb(0x121218));
         backgroundGradient.setCoordinateMode(QGradient::ObjectBoundingMode);
         m_chartBackgroundGradient = backgroundGradient;
 
         // Axes and other
         m_masterFont = QFont();
-        m_axisLinePen = QPen(QRgb(0xf7f7ff));
+        m_axisLinePen = QPen(QRgb(0x86878c));
         m_axisLinePen.setWidth(2);
-        m_axisLabelBrush = QBrush(QRgb(0xf7f7ff));
+        m_axisLabelBrush = QBrush(QRgb(0x86878c));
         m_axisLabelPen = Qt::NoPen; // NoPen for performance reasons
         m_backgroundShadesPen = Qt::NoPen;
         m_backgroundShades = BackgroundShadesNone;
-        m_gridLinePen = QPen(QRgb(0xf7f7ff));
+        m_gridLinePen = QPen(QRgb(0x86878c));
         m_gridLinePen.setWidth(1);
     }
 };
