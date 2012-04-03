b345b831b8c14dcb1857b93289244d4c7d061d63
Jani Honkonen <jani.honkonen@digia.com>
2012-04-03 10:05:21 +0300

    Add/modify license headers

--- qmlplugin/declarativebarseries.cpp
+++ qmlplugin/declarativebarseries.cpp
@@ -1,3 +1,23 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Digia Plc
+** All rights reserved.
+** For any questions to Digia, please use contact form at http://qt.digia.com
+**
+** This file is part of the Qt Commercial Charts Add-on.
+**
+** $QT_BEGIN_LICENSE$
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.
+**
+** If you have questions regarding the use of this file, please use
+** contact form at http://qt.digia.com
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #include "declarativebarseries.h"
 #include "declarativechart.h"
 #include "qchart.h"
