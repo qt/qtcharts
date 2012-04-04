aca72a0e3876d785a07316f5026df849b472a85e
sauimone <samu.uimonen@digia.com>
2012-04-04 17:44:16 +0300

    nice numbers on for barchart examples

--- examples/stackedbarchart/main.cpp
+++ examples/stackedbarchart/main.cpp
@@ -68,6 +68,7 @@ int main(int argc, char *argv[])
 //![5]
     chart->legend()->setVisible(true);
     chart->legend()->setAlignmnent(QLegend::AlignmentBottom);
+    chart->axisY()->setNiceNumbers(true);
 //![5]
 
 //![6]
