03d5855c29bc2a1f6649fbccb6d18b853bcec9bf
Jani Honkonen <jani.honkonen@digia.com>
2012-05-09 14:43:11 +0300

    Adding mac support for Bamboo build script

--- test/scripts/build.pl
+++ test/scripts/build.pl
@@ -15,25 +15,59 @@ my %job = Jobs::get($inifile, $jobname);
 
 # examine the platform
 given ($job{'Platform'}) {
+	
 	when ("Win7") {
+		
 		# construct a build command	
-		my @cmd;
 		if ($job{'ToolChain'} eq "mingw") {
-			@cmd = ($scriptdir . "\\build_win_mingw.bat", $job{'QtDir'}, $job{'Config'}, $job{'MinGWDir'});
+			run($scriptdir . "\\build_win_mingw.bat", $job{'QtDir'}, $job{'Config'}, $job{'MinGWDir'});
 		}
-		if ($job{'ToolChain'} eq "vs2005") {
-			@cmd = ($scriptdir . "\\build_win_vs2005.bat", $job{'QtDir'}, $job{'Config'});
+		elsif ($job{'ToolChain'} eq "vs2005") {
+			run($scriptdir . "\\build_win_vs2005.bat", $job{'QtDir'}, $job{'Config'});
 		}
-		if ($job{'ToolChain'} eq "vs2008") {
-			@cmd = ($scriptdir . "\\build_win_vs2008.bat", $job{'QtDir'}, $job{'Config'});
+		elsif ($job{'ToolChain'} eq "vs2008") {
+			run($scriptdir . "\\build_win_vs2008.bat", $job{'QtDir'}, $job{'Config'});
 		}
-		if ($job{'ToolChain'} eq "vs2010") {
-			@cmd = ($scriptdir . "\\build_win_vs2010.bat", $job{'QtDir'}, $job{'Config'});
+		elsif ($job{'ToolChain'} eq "vs2010") {
+			run($scriptdir . "\\build_win_vs2010.bat", $job{'QtDir'}, $job{'Config'});
 		}
-		# run the build command
-		system (@cmd) == 0 or die "system @cmd failed: $?";
+		else {
+			die "Unknown toolchain!";
+		}
 	}
+	
+	when ("Mac") {
+		
+		# setup build environment
+		$ENV{'QTDIR'} = $job{'QtDir'};
+		$ENV{'PATH'} = $job{'QtDir'} . "/bin:" . $ENV{'PATH'};
+		
+		# run qmake
+		my $cmd;
+		if ($job{'ToolChain'} eq "clang") {
+			run("qmake -r -spec unsupported/macx-clang CONFIG+=" . $job{'Config'});
+		}
+		elsif ($job{'ToolChain'} eq "gcc") {
+			run("qmake -r CONFIG+=" . $job{'Config'});
+		}
+		else {
+			die "Unknown toolchain!";
+		}
+		
+		# run make
+		run("make");
+	}
+	
 	default {
 		die "Unknown platform " . $job{'Platform'};
 	}
+}
+
+sub run {
+	my $cmd;
+	foreach (@_) {
+		$cmd .= "$_ ";
+	}
+	print "running : $cmd\n";
+	system(@_) == 0 or die "system \"$cmd\" failed: $?";
 }
