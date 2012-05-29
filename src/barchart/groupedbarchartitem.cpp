500f6258ec3ab95c28a8f2b619dd0e89d21415f5
sauimone <samu.uimonen@digia.com>
2012-05-29 16:11:58 +0300

    margin calculations in groupedbarchart

--- src/barchart/groupedbarchartitem.cpp
+++ src/barchart/groupedbarchartitem.cpp
@@ -47,7 +47,7 @@ QVector<QRectF> GroupedBarChartItem::calculateLayout()
     qreal scaleY = (height / rangeY);
     qreal scaleX = (width / rangeX);
     qreal categoryWidth = width / categoryCount;
-    qreal barWidth = categoryWidth / setCount - categoryWidth * m_series->d_func()->barMargin();
+    qreal barWidth = categoryWidth / setCount - (categoryWidth / setCount) * m_series->d_func()->barMargin();
 
     int itemIndex(0);
     for (int category = 0; category < categoryCount; category++) {
