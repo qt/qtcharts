785353bddfc1e54fe08a40b70026752415caee40
Marek Rosa <marek.rosa@digia.com>
2012-06-01 18:10:09 +0300

    Added license text to mapper classes

--- src/xychart/qxymodelmapper.h
+++ src/xychart/qxymodelmapper.h
@@ -1,3 +1,23 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Digia Plc
+** All rights reserved.
+** For any questions to Digia, please use contact form at http://qt.digia.com
+**
+** This file is part of the Qt Commercial Charts Add-on.
+**
+** $QT_BEGIN_LICENSE$
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.
+**
+** If you have questions regarding the use of this file, please use
+** contact form at http://qt.digia.com
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #ifndef QXYMODELMAPPER_H
 #define QXYMODELMAPPER_H
 
@@ -20,6 +40,9 @@ class QTCOMMERCIALCHART_EXPORT QXYModelMapper : public QObject
     Q_PROPERTY(int count READ count WRITE setCount)
     Q_ENUMS(Qt::Orientation)
 
+protected:
+    explicit QXYModelMapper(QObject *parent = 0);
+
 public:
     QAbstractItemModel* model() const;
     void setModel(QAbstractItemModel *model);
@@ -36,8 +59,6 @@ public:
     void reset();
 
 protected:
-    explicit QXYModelMapper(QObject *parent = 0);
-
     Qt::Orientation orientation() const;
     void setOrientation(Qt::Orientation orientation);
 
