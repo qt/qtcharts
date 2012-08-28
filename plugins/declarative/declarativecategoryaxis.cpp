10d3328dc3603d2c6ea9bb773e49262d5d65ba8f
Tero Ahola <tero.ahola@digia.com>
2012-08-28 14:00:37 +0300

    Added missing sources to declarative plugin

---
+++ plugins/declarative/declarativecategoryaxis.cpp
@@ -0,0 +1,87 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Digia Plc
+** All rights reserved.
+** For any questions to Digia, please use contact form at http://qt.digia.com
+**
+** This file is part of the Qt Commercial Charts Add-on.
+**
+** $QT_BEGIN_LICENSE$
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.
+**
+** If you have questions regarding the use of this file, please use
+** contact form at http://qt.digia.com
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "declarativecategoryaxis.h"
+#include <QDebug>
+
+QTCOMMERCIALCHART_BEGIN_NAMESPACE
+
+/*!
+    \qmlclass CategoryRange
+    \brief With CategoryRange you can define a range used by a CategoryAxis.
+    \sa CategoryAxis
+*/
+
+DeclarativeCategoryRange::DeclarativeCategoryRange(QObject *parent) :
+    QObject(parent),
+    m_endValue(0),
+    m_label(QString())
+{
+}
+
+DeclarativeCategoryAxis::DeclarativeCategoryAxis(QObject *parent) :
+    QCategoryAxis(parent)
+{
+}
+
+void DeclarativeCategoryAxis::classBegin()
+{
+}
+
+void DeclarativeCategoryAxis::componentComplete()
+{
+    foreach(QObject *child, children()) {
+        if (qobject_cast<DeclarativeCategoryRange *>(child)) {
+            DeclarativeCategoryRange *range = qobject_cast<DeclarativeCategoryRange *>(child);
+            append(range->label(), range->endValue());
+        }
+    }
+}
+
+QDeclarativeListProperty<QObject> DeclarativeCategoryAxis::axisChildren()
+{
+    return QDeclarativeListProperty<QObject>(this, 0, &DeclarativeCategoryAxis::appendAxisChildren);
+}
+
+void DeclarativeCategoryAxis::append(const QString &label, qreal categoryEndValue)
+{
+    QCategoryAxis::append(label, categoryEndValue);
+}
+
+void DeclarativeCategoryAxis::remove(const QString &label)
+{
+    QCategoryAxis::remove(label);
+}
+
+void DeclarativeCategoryAxis::replace(const QString& oldLabel, const QString& newLabel)
+{
+    QCategoryAxis::replaceLabel(oldLabel, newLabel);
+}
+
+void DeclarativeCategoryAxis::appendAxisChildren(QDeclarativeListProperty<QObject> *list, QObject *element)
+{
+    // Empty implementation; the children are parsed in componentComplete instead
+    Q_UNUSED(list)
+    Q_UNUSED(element)
+}
+
+#include "moc_declarativecategoryaxis.cpp"
+
+QTCOMMERCIALCHART_END_NAMESPACE
