f16c119125f6f9f6e881461f2aea357f79cec644
Jani Honkonen <jani.honkonen@digia.com>
2012-09-07 16:27:12 +0300

    remove tools from release

--- tools/build_win_vs2008.bat
+++
@@ -1,8 +0,0 @@
-set QTDIR=%1
-set PATH=%1\bin;%PATH%;
-set QMAKESPEC=win32-msvc2008
-call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
-@echo on
-qmake -r charts.pro CONFIG+=%2
-@echo on
-nmake
