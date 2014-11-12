377e4516d0365d65b87bba8704eeff0bf36e8478
Titta Heikkala <titta.heikkala@theqtcompany.com>
2014-11-12 14:08:11 +0200

    Updated license headers
    Updated new license headers and added missing ones to qdoc files.
    
    Change-Id: I9f2af2a8b44c3ebf19fd494628dfaa8775a47b06
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@theqtcompany.com>

--- examples/charts/donutbreakdown/donutbreakdownchart.h
+++ examples/charts/donutbreakdown/donutbreakdownchart.h
@@ -2,19 +2,17 @@
 **
 ** Copyright (C) 2014 Digia Plc
 ** All rights reserved.
-** For any questions to Digia, please use contact form at http://qt.digia.com
+** For any questions to Digia, please use contact form at http://qt.io
 **
-** This file is part of the Qt Enterprise Charts Add-on.
+** This file is part of the Qt Charts module.
 **
-** $QT_BEGIN_LICENSE$
-** Licensees holding valid Qt Enterprise licenses may use this file in
-** accordance with the Qt Enterprise License Agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Digia.
+** Licensees holding valid commercial license for Qt may use this file in
+** accordance with the Qt License Agreement provided with the Software
+** or, alternatively, in accordance with the terms contained in a written
+** agreement between you and Digia.
 **
 ** If you have questions regarding the use of this file, please use
-** contact form at http://qt.digia.com
-** $QT_END_LICENSE$
+** contact form at http://qt.io
 **
 ****************************************************************************/
 #ifndef DONUTBREAKDOWNCHART_H
