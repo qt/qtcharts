bc1f6aa59d423d1069eb732c165838a0a33b1daf
Titta Heikkala <titta.heikkala@theqtcompany.com>
2015-04-23 13:51:15 +0300

    Copyright header changes
    Copyright header changes for Qt 5.5.
    
    Change-Id: I2b8e9356997de2c87a7436bcefd4d0948d566259
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@theqtcompany.com>

--- tests/auto/qdatetimeaxis/tst_qdatetimeaxis.cpp
+++ tests/auto/qdatetimeaxis/tst_qdatetimeaxis.cpp
@@ -1,15 +1,15 @@
 /****************************************************************************
  **
- ** Copyright (C) 2014 Digia Plc
+ ** Copyright (C) 2015 The Qt Company Ltd
  ** All rights reserved.
- ** For any questions to Digia, please use contact form at http://qt.io
+ ** For any questions to The Qt Company, please use contact form at http://qt.io
  **
  ** This file is part of the Qt Charts module.
  **
  ** Licensees holding valid commercial license for Qt may use this file in
  ** accordance with the Qt License Agreement provided with the Software
  ** or, alternatively, in accordance with the terms contained in a written
- ** agreement between you and Digia.
+ ** agreement between you and The Qt Company.
  **
  ** If you have questions regarding the use of this file, please use
  ** contact form at http://qt.io
