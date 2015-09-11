a9a78a1d08bc5082555480a4734d762c93dccc95
Titta Heikkala <titta.heikkala@theqtcompany.com>
2015-09-11 12:18:16 +0300

    Updated private header warning
    Removed 'Enterprise' from the Chart API name.
    
    Change-Id: Id6c7f3896bfcd72da2deebee430826b9c25793f6
    Task-number: QTRD-3511
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@theqtcompany.com>

--- src/charts/animations/baranimation_p.h
+++ src/charts/animations/baranimation_p.h
@@ -19,7 +19,7 @@
 //  W A R N I N G
 //  -------------
 //
-// This file is not part of the Qt Enterprise Chart API.  It exists purely as an
+// This file is not part of the Qt Chart API.  It exists purely as an
 // implementation detail.  This header file may change from version to
 // version without notice, or even be removed.
 //
