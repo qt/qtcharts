9700d6ce42d4e2a644f3ad91bdbd2448e1d50feb
Friedemann Kleint <Friedemann.Kleint@qt.io>
2018-02-07 15:47:05 +0100

    Polish the audio example
    - Use C++ 11 (nullptr, override)
    - Bail out cleanly if there is no audio input available
    - Rewrite the buffer algorithm to work on a member
      variable to make the slidling window principle
      clearer and reduce QVector allocations
    
    Task-number: QTBUG-60662
    Change-Id: I02827f3dca4f0bf1fb213fa12369455de6c618c5
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>

--- examples/charts/audio/widget.h
+++ examples/charts/audio/widget.h
@@ -44,6 +44,7 @@ class XYSeriesIODevice;
 
 QT_BEGIN_NAMESPACE
 class QAudioInput;
+class QAudioDeviceInfo;
 QT_END_NAMESPACE
 
 class Widget : public QWidget
@@ -51,14 +52,14 @@ class Widget : public QWidget
     Q_OBJECT
 
 public:
-    Widget(QWidget *parent = 0);
+    explicit Widget(const QAudioDeviceInfo &deviceInfo, QWidget *parent = nullptr);
     ~Widget();
 
 private:
-    XYSeriesIODevice *m_device;
+    XYSeriesIODevice *m_device = nullptr;
     QChart *m_chart;
-    QLineSeries *m_series;
-    QAudioInput *m_audioInput;
+    QLineSeries *m_series ;
+    QAudioInput *m_audioInput = nullptr;
 };
 
 #endif // WIDGET_H
--- examples/charts/audio/xyseriesiodevice.h
+++ examples/charts/audio/xyseriesiodevice.h
@@ -31,6 +31,8 @@
 #define XYSERIESIODEVICE_H
 
 #include <QtCore/QIODevice>
+#include <QtCore/QPointF>
+#include <QtCore/QVector>
 #include <QtCharts/QChartGlobal>
 
 QT_CHARTS_BEGIN_NAMESPACE
@@ -43,14 +45,17 @@ class XYSeriesIODevice : public QIODevice
 {
     Q_OBJECT
 public:
-    explicit XYSeriesIODevice(QXYSeries * series, QObject *parent = 0);
+    explicit XYSeriesIODevice(QXYSeries *series, QObject *parent = nullptr);
+
+    static const int sampleCount = 2000;
 
 protected:
-    qint64 readData(char * data, qint64 maxSize);
-    qint64 writeData(const char * data, qint64 maxSize);
+    qint64 readData(char *data, qint64 maxSize) override;
+    qint64 writeData(const char *data, qint64 maxSize) override;
 
 private:
     QXYSeries *m_series;
+    QVector<QPointF> m_buffer;
 };
 
 #endif // XYSERIESIODEVICE_H
