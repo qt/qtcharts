166eae80efe685ffbc7097ebdbe3e68d12e7d2e0
Miikka Heikkinen <miikka.heikkinen@qt.io>
2019-06-18 13:10:30 +0300

    Force entire chart update when points label clipping changes
    Series points labels can be partly outside the plot area.
    When they are set to be clipped to plot area, the parts of labels
    that are outside the plot area are not cleared unless the whole chart
    is updated.
    
    Task-number: QTBUG-60384
    Change-Id: I67233b562d9d0e404c09e5b661123819e97fc529
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- src/charts/linechart/linechartitem.cpp
+++ src/charts/linechart/linechartitem.cpp
@@ -354,12 +354,18 @@ void LineChartItem::handleUpdated()
     m_pointLabelsVisible = m_series->pointLabelsVisible();
     m_pointLabelsFont = m_series->pointLabelsFont();
     m_pointLabelsColor = m_series->pointLabelsColor();
+    bool labelClippingChanged = m_pointLabelsClipping != m_series->pointLabelsClipping();
     m_pointLabelsClipping = m_series->pointLabelsClipping();
     if (doGeometryUpdate)
         updateGeometry();
     else if (m_series->useOpenGL() && visibleChanged)
         refreshGlChart();
-    update();
+
+    // Update whole chart in case label clipping changed as labels can be outside series area
+    if (labelClippingChanged)
+        m_series->chart()->update();
+    else
+        update();
 }
 
 void LineChartItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
