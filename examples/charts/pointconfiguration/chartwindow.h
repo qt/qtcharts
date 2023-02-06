f8770bf02180da5b593e3c32859387fbe6f6f0f3
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-02-06 13:58:38 +0100

    pointconfiguration example: Add missing include guards
    Amends 189f798b2c44b3a061d8b1275ad03f5e5ef29486.
    
    Task-number: QTBUG-109394
    Change-Id: I7ae2db05e29ae84b543c6b37ed1c54693ec66541
    Reviewed-by: Keith Kyzivat <keith.kyzivat@qt.io>
    Reviewed-by: Amir Masoud Abdol <amir.abdol@qt.io>
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>
    (cherry picked from commit cc2f7547d1beb51257a19b40c8e87e347044c7b6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/charts/pointconfiguration/chartwindow.h
+++ examples/charts/pointconfiguration/chartwindow.h
@@ -1,6 +1,9 @@
 // Copyright (C) 2022 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
+#ifndef CHARTWINDOW_H
+#define CHARTWINDOW_H
+
 #include <QChartView>
 #include <QCheckBox>
 #include <QComboBox>
@@ -34,3 +37,5 @@ private:
     QLineEdit *m_customLabelLineEdit = nullptr;
 };
 //![1]
+
+#endif // CHARTWINDOW_H
