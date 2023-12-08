d064dbf2d152302f319ed4a3afd784bb17d82ed2
Volker Hilsheimer <volker.hilsheimer@qt.io>
2023-12-08 16:11:28 +0100

    Unbreak wheel event handling in QChartView
    Reverts in practice e757445355dbb3f3001fae49bb72113d7a52173d, which
    removed wheel event handling in QChartView completely, and - by not
    even explicitly ignoring the event - broke event propagation to a parent
    that might want to handle the wheel event (such as a parent scrollview).
    
    Testing the reproducer from QTBUG-77403 shows that the problem stays
    fixed anyway, probably through the general fixes to wheel-event handling
    and propagation in 92df790f46b3a8b17aec2f385d6472fd3f8647f6.
    
    Fixes: QTBUG-119712
    Task-number: QTBUG-77403
    Pick-to: 6.6 6.5
    Change-Id: Icb94172659e0c4f41d7538d1e2432b6e4c548afe
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>

--- src/charts/qchartview.cpp
+++ src/charts/qchartview.cpp
@@ -250,9 +250,7 @@ void QChartView::mouseReleaseEvent(QMouseEvent *event)
 #if QT_CONFIG(wheelevent)
 void QChartView::wheelEvent(QWheelEvent *event)
 {
-    Q_UNUSED(event);
-    // We just need to override wheelEvent, or scrolling won't work correctly on macOS trackpad
-    // (QTBUG-77403)
+    QGraphicsView::wheelEvent(event);
 }
 #endif
 #endif
