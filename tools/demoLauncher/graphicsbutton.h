8b27e22b7b64d23b395a5f796a30b78c6fc39f89
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-01-02 13:59:47 +0200

    Revert "Update commercial SPDX-License-Identifier to files under \tools"
    This reverts commit 93283eeccf5d5fc11b903eb44acfcc675a5c9953.
    
    Revert of commercial SPDX license identifiers is required for the
    Qt 6.5.x opensource releases, Qt 6.5.4 onwards.
    
    Change-Id: I4d945c009fff1d50f43187b4977ddae107c91ebf
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- tools/demoLauncher/graphicsbutton.h
+++ tools/demoLauncher/graphicsbutton.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 
 #ifndef GRAPHICSBUTTON_H
 #define GRAPHICSBUTTON_H
