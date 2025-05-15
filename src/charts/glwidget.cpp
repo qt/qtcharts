b2c143c79e1b0ec755879918b8e93b6f95f5e00f
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-05-15 22:34:01 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the source files. Examples, tests, documentation,
    or build files are not updated.
    
    Modified licenseRule.json and REUSE.toml accordingly.
    
    Task-number: QTBUG-136484
    Change-Id: I9778c16025866622530b6dd171730349e227e8d9
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>

--- src/charts/glwidget.cpp
+++ src/charts/glwidget.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #undef QT_NO_FOREACH // this file contains unported legacy Q_FOREACH uses
 
